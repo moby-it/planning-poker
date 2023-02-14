76ab68315cd52591bfe8205488eb167fef8881ac
George Spanos <gespa11019@gmail.com>
2023-02-14 20:32:52 +0200

    remove notify from room

--- web/round.go
+++ web/round.go
@@ -11,32 +11,16 @@ type Round struct {
 	Votes map[string]int
 }
 
-func NewRound() *Round {
-	return &Round{Votes: make(map[string]int), Room: nil}
+func NewRound(room *Room) *Round {
+	return &Round{Votes: make(map[string]int), Room: room}
 }
 func (r Round) isReadyForReveal() bool {
 	return len(r.Room.Voters) == len(r.Votes)
 }
-func (r Round) notifyUsers() error {
-	var err error
-	for _, client := range r.Room.Voters {
-		err = client.Connection.WriteJSON(r.Votes)
-		if err != nil {
-			return err
-		}
-	}
-	for _, client := range r.Room.Spectators {
 
-		err = client.Connection.WriteJSON(r.Votes)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
 func (r Round) revealRound() error {
 	if r.isReadyForReveal() {
-		err := r.notifyUsers()
+		err := r.Room.notifyUsers()
 		return err
 	} else {
 		return fmt.Errorf("not every user has voted")
