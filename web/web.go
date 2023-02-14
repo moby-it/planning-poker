65a89ada3bcc264f2896ab9d4cc2de03bffdbce0
George Spanos <gespa11019@gmail.com>
2023-02-14 20:33:52 +0200

    add pprof

--- web/web.go
+++ web/web.go
@@ -12,12 +12,13 @@ const prefix = "/api"
 
 func StartApp() error {
 	r := mux.NewRouter()
+	// attachProfiler(r)
 	apiRouter := r.PathPrefix(prefix).Subrouter()
 
 	// v1 Handlers
 	v1Router := apiRouter.PathPrefix("/v1").Subrouter()
 	v1Router.HandleFunc("/createRoom", CreateRoom).Methods("POST")
-	v1Router.HandleFunc("/joinRoom/{roomId}/{username}", ConnectToRoom).Methods("GET")
+	v1Router.HandleFunc("/joinRoom/{roomId}/{username}", ConnectToRoom)
 
 	srv := &http.Server{
 		Handler: r,
@@ -29,3 +30,11 @@ func StartApp() error {
 	log.Println("Listening on port 8080")
 	return srv.ListenAndServe()
 }
+
+// func attachProfiler(router *mux.Router) {
+// 	router.HandleFunc("/debug/pprof/", pprof.Index)
+// 	router.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+// 	router.HandleFunc("/debug/pprof/profile", pprof.Profile)
+// 	router.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+// 	router.HandleFunc("/debug/pprof/trace", pprof.Trace)
+// }
