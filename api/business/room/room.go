0e2052c6a50bfcf7eb689e1db02e7327a25bf4a7
George Spanos <gespa11019@gmail.com>
2023-03-20 01:11:03 +0200

    replace lock with Rlock

--- api/business/room/room.go
+++ api/business/room/room.go
@@ -29,7 +29,7 @@ type Room struct {
 	Spectators   []*user.Connection
 	CurrentRound *Round
 	cancelReveal chan bool
-	Mu           sync.Mutex
+	Mu           sync.RWMutex
 }
 
 var rooms = make(map[string]*Room)
@@ -61,9 +61,9 @@ func GetLength() int {
 	return len(rooms)
 }
 func (room *Room) UserHasVoted(username string) bool {
-	room.Mu.Lock()
+	room.Mu.RLock()
 	_, found := room.CurrentRound.Votes[username]
-	room.Mu.Unlock()
+	room.Mu.RUnlock()
 	return found
 }
 func (room *Room) IncludeUsername(username string) bool {
@@ -103,14 +103,14 @@ func (room *Room) IsEmpty() bool {
 }
 
 func (r *Room) Close() {
-	r.Mu.Lock()
-	defer r.Mu.Unlock()
+	r.Mu.RLock()
+	defer r.Mu.RUnlock()
 	delete(rooms, r.Id)
 }
 func (room *Room) Vote(username string, storyPoints int) {
-	room.Mu.Lock()
+	room.Mu.RLock()
 	room.CurrentRound.Votes[username] = storyPoints
-	room.Mu.Unlock()
+	room.Mu.RUnlock()
 	event := events.UserVotedEvent{Username: username, Event: events.Event{Type: events.UserVoted}}
 	events.Broadcast(event, room.Connections()...)
 	revealEvent := events.RoundRevealAvailableEvent{Event: events.Event{Type: events.RoundRevealAvailable}, RevealAvailable: room.CurrentRound.IsRevealable(len(room.Voters))}
@@ -135,7 +135,7 @@ func (room *Room) AddClient(client *user.Connection, role string) error {
 }
 
 func (room *Room) removeClient(client *user.Connection) {
-	room.Mu.Lock()
+	room.Mu.RLock()
 	for i, c := range room.Voters {
 		if c == client {
 			room.Voters = append(room.Voters[:i], room.Voters[i+1:]...)
@@ -147,7 +147,7 @@ func (room *Room) removeClient(client *user.Connection) {
 			room.Spectators = append(room.Spectators[:i], room.Spectators[i+1:]...)
 		}
 	}
-	room.Mu.Unlock()
+	room.Mu.RUnlock()
 	room.emitUsersAndRevealableRound()
 	log.Printf("%v left room %v", client.Username, room.Id)
 }
@@ -169,9 +169,9 @@ func (room *Room) emitUsersAndRevealableRound() {
 	}
 	event := events.UsersUpdatedEvent{Users: users, Event: events.Event{Type: events.UsersUpdated}}
 	events.Broadcast(event, room.Connections()...)
-	room.Mu.Lock()
+	room.Mu.RLock()
 	revealableEvent := events.RoundRevealAvailableEvent{Event: events.Event{Type: events.RoundRevealAvailable}, RevealAvailable: room.CurrentRound.IsRevealable(len(room.Voters))}
-	room.Mu.Unlock()
+	room.Mu.RUnlock()
 	if room.cancelReveal != nil {
 		room.cancelReveal <- true
 	}
@@ -218,9 +218,9 @@ func (room *Room) readMessage(client *user.Connection) {
 			event := events.RoundToRevealEvent{Event: events.Event{Type: events.RoundToReveal}, After: 5000} // after in ms
 			events.Broadcast(event, room.Connections()...)
 			reveal, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-			room.Mu.Lock()
+			room.Mu.RLock()
 			room.cancelReveal = make(chan bool, 1)
-			room.Mu.Unlock()
+			room.Mu.RUnlock()
 			go func() {
 				select {
 				case <-room.cancelReveal:
@@ -231,9 +231,9 @@ func (room *Room) readMessage(client *user.Connection) {
 				case <-reveal.Done():
 					room.RevealCurrentRound()
 				}
-				room.Mu.Lock()
+				room.Mu.RLock()
 				room.cancelReveal = nil
-				room.Mu.Unlock()
+				room.Mu.RUnlock()
 			}()
 		case actions.CancelReveal:
 			room.cancelReveal <- true
