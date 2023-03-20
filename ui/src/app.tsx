1fe8b33cda6b86bb82671b6643dd4cee4ec7a2b7
George Spanos <gespa11019@gmail.com>
2023-03-20 15:47:52 +0200

    fix component redirect

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,5 +1,5 @@
-import { Route, Routes } from "@solidjs/router";
-import { lazy } from "solid-js";
+import { Route, Routes, useNavigate } from "@solidjs/router";
+import { Component, lazy } from "solid-js";
 import toast, { Toaster } from "solid-toast";
 import { Header } from "./components/header/header";
 
@@ -14,8 +14,14 @@ export const App = () => {
         <Route path="/" component={Home} />
         <Route path="/prejoin" component={PrejoinForm} />
         <Route path="/room/:roomId" component={Room} />
+        <Route path="*" component={NoComponent}></Route>
       </Routes>
       <Toaster position="top-right" gutter={8} />
     </>
   );
+};
+const NoComponent: Component = () => {
+  const navigate = useNavigate();
+  navigate("/");
+  return <></>;
 };
