2edd8b74936ec048b7d8515d20210e6d76beadc0
George Spanos <gespa11019@gmail.com>
2023-04-04 19:53:04 +0300

    remove unused code from ui config

--- ui/src/config.ts
+++ ui/src/config.ts
@@ -1,4 +1,3 @@
-import { createEffect, createRoot, createSignal } from "solid-js";
 
 export const apiV1Url = import.meta.env.VITE_API_URL + "/v1";
 export const wsv1Url = import.meta.env.VITE_WS_URL + "/v1";
