b5911e637966f96291193c9d286dfb7239497850
George Spanos <gspanos@moby-it.com>
2023-09-05 00:55:47 +0300

    poker planning vanilla 1st RC

--- web/web/static/js/messageHandler.js
+++ web/web/static/js/messageHandler.js
@@ -20,7 +20,7 @@ function handleWsMessage(message) {
       break;
     case 'roundToReveal':
       console.log('round to reveal after', data.after);
-      // roundToReveal();
+      roundToReveal(data.after);
       break;
     case 'cancelReveal':
       console.log('cancel reveal');
@@ -42,7 +42,7 @@ function updateUsers(users) {
   const board = document.querySelector('.board');
   while (board.children.length > 0) board.removeChild(board.lastChild);
   const spectatorsList = document.querySelector('ul.spectators');
-  while (spectatorsList.children.length > 2) spectatorsList.removeChild(spectatorsList.lastChild);
+  while (spectatorsList.children.length >= 2) spectatorsList.removeChild(spectatorsList.lastChild);
   for (const user of users) {
     if (user.isVoter) {
       addVoter(user);
@@ -79,6 +79,8 @@ function resetRound() {
   document.querySelectorAll('.card.voted').forEach(e => e.classList.remove('voted'));
   document.querySelector('.voting-card.selected').classList.remove('selected');
   document.querySelectorAll('.reveal').forEach(e => e.removeChild(e.lastChild));
+  document.querySelector('#progress-bar').style.display = 'none';
+  setHeader("Everyone's Ready");
 }
 function revealRound(votes) {
   document.querySelectorAll('.card.voted').forEach(e => e.classList.remove('voted'));
@@ -88,12 +90,44 @@ function revealRound(votes) {
     storyPoints.innerText = pointsVoted;
     vote.appendChild(storyPoints);
   }
+  // show average
+  const voteSum = Object.values(votes).reduce((prev, curr) => prev + curr);
+  const average = voteSum / Object.keys(votes).length;
+  setHeader('Average Story Points ' + average);
+  // change button
   const submitButton = document.querySelector('.btn.primary');
   submitButton.innerText = "Start New Round";
   submitButton.removeEventListener('click', handleRevealSubmit);
   submitButton.addEventListener('click', handleRoundToStart);
 }
-function roundToReveal() {
+function roundToReveal(after) {
+  // add progress bar
+  const progressBar = document.querySelector('#progress-bar');
+  if (progressBar) progressBar.style.display = 'block';
+  if (!after) after = 5000;
+  let width = 1;
+  let i = 1;
+  const intervalTime = 10;
+  const interval = setInterval(() => {
+    if (width >= 100) clearInterval(interval);
+    width = ((i * intervalTime) / after) * 100;
+    document.querySelector('#progress-bar #bar').style.width = width + '%';
+    i++;
+  }, intervalTime);
+
+  // set room header
+  setHeader(`Revealing in ${after / 1000}`);
+  let remainingTime = after - 1000;
+  const headerInterval = setInterval(() => {
+    if (remainingTime < 0) {
+      clearInterval(headerInterval);
+      return;
+    }
+    const headerText = `Revealing in ${remainingTime / 1000}`;
+    remainingTime = remainingTime - 1000;
+    if (remainingTime > 0)
+      setHeader(headerText);
+  }, 1000);
 
 }
 function updateRoundIsRevealable(revealAvailable) {
@@ -115,4 +149,8 @@ function handleRevealSubmit() {
 }
 function handleRoundToStart() {
   sendWsMessage({ type: 'roundToStart' });
+}
+function setHeader(header) {
+  const headerText = document.querySelector('.room-header > h2');
+  headerText.innerText = header;
 }
