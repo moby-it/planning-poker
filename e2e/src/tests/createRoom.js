d2f470c51904f1c36d779e20a9f3034b85c2858a
George Spanos <gspanos@moby-it.com>
2023-09-05 18:06:11 +0300

    make e2e suite compat with MPA

--- e2e/src/tests/createRoom.js
+++ e2e/src/tests/createRoom.js
@@ -1,23 +1,24 @@
 import {
   FillCreateRoomForm,
   NavigateToHome,
-  SubmitCreateRoomForm,
+  SubmitCreateRoomForm
 } from "../helpers/commands.js";
 
-import { queries, waitFor } from "pptr-testing-library";
+import { getDocument, queries } from "pptr-testing-library";
 import { createBrowser } from "../helpers/foundation.js";
 
 const { getByTestId } = queries;
 
 export const createRoom = async (username, role = "voter") => {
   const browser = await createBrowser();
-  const { $document, page } = await NavigateToHome(browser);
+  let { $document, page } = await NavigateToHome(browser);
   const startBtn = await getByTestId($document, "start-here");
   await startBtn.click();
-  await waitFor(() => getByTestId($document, "create-room"));
+  await page.waitForTestId('create-room');
+  $document = await getDocument(page);
   await FillCreateRoomForm($document, username, role);
   await SubmitCreateRoomForm($document);
-  await waitFor(() => getByTestId($document, "room"));
+  await page.waitForTestId('room');
   const roomId = page.url().split("/").pop();
   return [roomId, { $document, browser }];
 };
--- e2e/src/tests/voterJoinsRoom.js
+++ e2e/src/tests/voterJoinsRoom.js
@@ -1,18 +1,18 @@
+import { getDocument } from "pptr-testing-library";
 import {
   FillCreateRoomForm,
   NavigateToRoom,
   SubmitCreateRoomForm,
 } from "../helpers/commands.js";
-import { queries, waitFor } from "pptr-testing-library";
 import { createBrowser } from "../helpers/foundation.js";
 
-const { getByTestId } = queries;
 export const voterJoinsRoom = async (roomId, name) => {
   const browser = await createBrowser();
-  const { $document } = await NavigateToRoom(browser, roomId);
-  await waitFor(() => getByTestId($document, "create-room"));
+  let { $document, page } = await NavigateToRoom(browser, roomId);
+  await page.waitForTestId('create-room');
+  $document = await getDocument(page);
   await FillCreateRoomForm($document, name);
   await SubmitCreateRoomForm($document);
-  await waitFor(() => getByTestId($document, "voting-card-list"));
+  await page.waitForTestId('voting-card-list');
   return { $document, browser };
 };
