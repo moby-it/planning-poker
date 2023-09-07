3c44fb542485af2ebe1d049ad2a5f6793dfd300d
George Spanos <gspanos@moby-it.com>
2023-09-07 20:23:04 +0300

    add compatibility notes

--- web/web/static/js/info.js
+++ web/web/static/js/info.js
@@ -10,13 +10,18 @@ function removePopupTimeout() {
       removePopupTimeout();
       return;
     }
-    if (hoverEl) hoverEl.remove();
+    if (hoverEl) {
+      hoverEl.remove();
+      hoverEl = null;
+    }
   }, 1000);
 }
 infoEl.addEventListener('mouseenter', () => {
-  const logo = document.querySelector('.logo');
-  hoverEl = createHoverTextEl();
-  logo.appendChild(hoverEl);
+  if (!hoverEl) {
+    const logo = document.querySelector('.logo');
+    hoverEl = createHoverTextEl();
+    logo.appendChild(hoverEl);
+  }
 });
 infoEl.addEventListener('mouseleave', () => {
   removePopupTimeout();
@@ -26,7 +31,11 @@ function createHoverTextEl() {
   const hoverText = document.createElement('span');
   hoverText.innerHTML = `We are testing a new version of the app. Should you find any bugs, please submit 
   an issue at <a class="moby-link" href="https://github.com/moby-it/planning-poker">our github project</a> 
-  or email us directly at <a class="moby-link" target="_blank" href="mailto:contact@moby-it.com">contact@moby-it.com</a>`
+  or email us directly at <a class="moby-link" target="_blank" href="mailto:contact@moby-it.com">contact@moby-it.com</a>
+  <br />
+  In case you have too many issues you can still use the old version at 
+  <a class="moby-link" href="https://v1.poker-planning.net/v1">https://v1.poker-planning.net/v1</a>
+  `
     ;
   hoverText.classList.add('warn-popup');
   return hoverText;
